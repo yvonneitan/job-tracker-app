cb60b2d72cad10ec5e734a4a17b68673ac69a439
Anoshka Jhaveri <anoshkaujhaveri@gmail.com>
2024-10-03 13:49:37 -0700

    added client files

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1 className="header__title">Job Hunt or Haunt?</h1>
+      <h3 className="header__subtitle">
+        --the ultimate job tracker for the brave...
+      </h3>
+      <Link to={`/`} className="header__link">
+        HomePage
+      </Link>
+      <Link to={`/active`} className="header__link">
+        Active Jobs
+      </Link>
+      <Link to={`/inactive`} className="header__link">
+        Inactive Jobs
+      </Link>
+    </div>
+  );
+}
+
+export default Header;
